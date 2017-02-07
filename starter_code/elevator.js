7c3580055fca1f64048b97e089c67ed3746a3f2e
Marta Delgado <marta.e.delgado@gmail.com>
2017-02-07 09:41:08 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,54 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
+    this.idle       = true;
+    this.waitingList =[];
+    this.passengers  = [];
+    this.currentReq = null;
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+  this.interval = setInterval(() => { this.update() }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+    this.log();
+  }
+
+
   _passengersEnter() { }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() {
+    if(this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+  }
+
+  floorDown() {
+    if(this.floor >= 0) {
+    this.floor--;
+    }
+
+  }
+
+  call() {
+      // this.waitingList.push(person);
+      // this.requests.push(person.originFloor);
+      // this.requests =_.sortBy(_.uniq(this.requests));
+  }
+
+
+  log() {
+    console.log(this.floor,this.direction);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,9 @@
 const Elevator = require('./elevator.js');
+const Person = require ('./person.js');
+
+let elevator = new Elevator();
+elevator.start();
+
+elevator.call(charlie);
+// elevator.call(thor);
+// elevator.call(matias);
